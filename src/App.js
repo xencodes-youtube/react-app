3232ef80493c2c79e13ef10d8aa5b45428734f59
xencodes <xencodes@icloud.com>
2021-05-24 12:07:44 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import { Button } from "@chakra-ui/button";
+import { Text } from "@chakra-ui/layout";
+import { Stack } from "@chakra-ui/layout";
+
+import { FiLink2 } from "react-icons/fi";
+
+const { Heading } = require("@chakra-ui/layout");
+const { Box } = require("@chakra-ui/layout");
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box>
+      <Box p={12} bgGradient="linear(to-r, purple.400, red.400)">
+        <Stack spacing={4}>
+          <Heading size="4xl" textColor="white">
+            React Application Example
+          </Heading>
+          <Text fontSize="xl" textColor="white">
+            Irure enim occaecat non sunt do culpa minim veniam ut fugiat
+            voluptate laborum amet tempor.
+          </Text>
+        </Stack>
+        <Button mt={6} leftIcon={<FiLink2 />}>
+          Learn more
+        </Button>
+      </Box>
+    </Box>
   );
 }
 
